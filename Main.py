455e5012b7cf60aecce9c867821ed462294421f8
WenShao233 <2911702319@qq.com>
2023-03-11 17:47:16 +0800

    modified:   Main.py

--- Main.py
+++ Main.py
@@ -6,18 +6,17 @@ class SetJavaVersion:
 
         self.JavaList = {}
         self.JavaVersionList = {}
-        self.JavaPath = {}
         self.AppData = os.environ.get("AppData")
 
         print("欢迎使用Java切换器! 建议以管理员身份运行, 在可输入的地方输入help查看帮助")
 
         with open(f"{self.AppData}\\SetJavaVersion\\JavaPath.json", "r+") as f1:
-            self.JavaPath = json.loads(f1.read(), strict = False)
+            self.JavaList = json.loads(f1.read(), strict = False)
 
         if not os.path.exists(f"{self.AppData}\\SetJavaVersion"):
             os.mkdir(f"{self.AppData}\\SetJavaVersion")
             self.FirstStart()
-        elif not os.path.exists(f"{self.AppData}\\SetJavaVersion\\JavaPath.json") or self.JavaPath == "":
+        elif not os.path.exists(f"{self.AppData}\\SetJavaVersion\\JavaPath.json") or self.JavaList == "":
             print("配置文件已丢失, 请重新配置")
             self.FirstStart()
         else:
@@ -53,30 +52,53 @@ Java路径保存位置:
             JavaVersion = input(f"请输入第{j + 1}个Java版本号(Java8就写8, Java17就写17): ")
             VersionPath = input(f"请输入第{j + 1}个Java目录(例: C:\Program Files\Java\Jre_8u231): ")
 
-        self.JavaPath[f"{JavaVersion}"] = VersionPath
+        self.JavaList[f"{JavaVersion}"] = VersionPath
         with open(f"{self.AppData}\\SetJavaVersion\\JavaPath.json", "w+") as f1:
-            json.dump(self.JavaPath, f1, indent = 4)
+            json.dump(self.JavaList, f1, indent = 4)
 
         self.SettingJavaVersion()
         
+    def GetNowJava(self):
+        import os
+        try:
+            if os.environ.get("JAVA_HOME") != None:
+                NowJavaPath = os.environ.get("JAVA_HOME")
+        finally:
+            t1 = 0
+            for NowJavaVersion in self.JavaList.keys():
+                if NowJavaVersion == NowJavaPath:
+                    print(f"当前Java为{NowJavaVersion}, 位于{NowJavaPath}")
+                    continue
+                else:
+                    t1 += 1
+        if t1 == len(self.JavaList):
+            print("当前JAVA变量不正常, 请立即更换!")
 
     def SettingJavaVersion(self):
 
         import os
         import time
 
-        version = input("请输入要切换的Java版本: ")
+        self.GetNowJava()
+        
+        while True:
+            try:
+                version = str(input("请输入要切换的Java版本: "))
+                self.JavaList{version}
+            except:
+                print("")
+            print("正在更改用户变量...")
+            os.system(f"setx \"JAVA_HOME\" \"{self.JavaList[version]}")
 
-        print("正在更改用户变量...")
-        os.system(f"setx \"JAVA_HOME\" \"{self.JavaPath[version]}")
+            print("\n")
 
-        print("\n")
+            print("正在更改系统变量...")
+            os.system(f"setx -m \"JAVA_HOME\" \"{self.JavaList[version]}")
 
-        print("正在更改系统变量...")
-        os.system(f"setx -m \"JAVA_HOME\" \"{self.JavaPath[version]}")
-
-        time.sleep(2)
-        exit()
+            time.sleep(2)
+            exit()
+            else:
+                print("当前没有该Java, 若您已安装该Java, 请输入add添加Java")
 
 if __name__ == "__main__":
     SetJavaVersion()
